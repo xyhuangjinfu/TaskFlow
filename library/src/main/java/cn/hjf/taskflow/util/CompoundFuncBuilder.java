b5ac0c7af6b79a89459dfdd5bc3847b0bad95fff
xyhuangjinfu <630569027@qq.com>
2020-03-28 15:34:14 +0800

    check compound param

--- library/src/main/java/cn/hjf/taskflow/util/CompoundFuncBuilder.java
+++ library/src/main/java/cn/hjf/taskflow/util/CompoundFuncBuilder.java
@@ -6,6 +6,8 @@ abstract class CompoundFuncBuilder {
     protected boolean mHaveEnd = false;
 
     public CompoundFuncBuilder(IFunc start) {
+        checkStart(start);
+
         mGraphBuilder = new GraphBuilder();
         mGraphBuilder.addFunc(start);
     }
@@ -16,10 +18,39 @@ abstract class CompoundFuncBuilder {
      * ***************************************************************************************************************
      */
 
-    protected void checkEnd() {
+    protected void checkStart(IFunc start) {
+        checkPre(start);
+    }
+
+    protected void checkEnd(IFunc end) {
+        checkPre(end);
+        checkNext(end);
+
         if (mHaveEnd) {
             throw new RuntimeException("addEnd can only be called once during one build process.");
         }
         mHaveEnd = true;
+    }
+
+    protected void checkNormal(IFunc func) {
+        checkPre(func);
+    }
+
+    /**
+     * ***************************************************************************************************************
+     * //
+     * ***************************************************************************************************************
+     */
+
+    private void checkPre(IFunc func) {
+        if (!func.getPreList().isEmpty()) {
+            throw new IllegalArgumentException("func: " + func + " already have pre func. " + func.getPreList());
+        }
+    }
+
+    private void checkNext(IFunc func) {
+        if (!func.getNextList().isEmpty()) {
+            throw new IllegalArgumentException("func: " + func + " already have next func. " + func.getNextList());
+        }
     }
 }
--- library/src/main/java/cn/hjf/taskflow/util/CompoundFuncBuilderN.java
+++ library/src/main/java/cn/hjf/taskflow/util/CompoundFuncBuilderN.java
@@ -29,73 +29,73 @@ public class CompoundFuncBuilderN<Return> extends CompoundFuncBuilder {
      */
 
     public CompoundFuncBuilderN<Return> addEnd(IFunc0<Return> end, IFunc<Void> pre1) {
-        checkEnd();
+        checkEnd(end);
         mGraphBuilder.addLink(end, pre1);
         return this;
     }
 
     public <P> CompoundFuncBuilderN<Return> addEnd(IFunc1<P, Return> end, IFunc<P> pre1) {
-        checkEnd();
+        checkEnd(end);
         mGraphBuilder.addLink(end, pre1);
         return this;
     }
 
     public <P1, P2> CompoundFuncBuilderN<Return> addEnd(IFunc2<P1, P2, Return> end, IFunc<P1> pre1, IFunc<P2> pre2) {
-        checkEnd();
+        checkEnd(end);
         mGraphBuilder.addLink(end, pre1, pre2);
         return this;
     }
 
     public <P1, P2, P3> CompoundFuncBuilderN<Return> addEnd(IFunc3<P1, P2, P3, Return> end, IFunc<P1> pre1, IFunc<P2> pre2, IFunc<P3> pre3) {
-        checkEnd();
+        checkEnd(end);
         mGraphBuilder.addLink(end, pre1, pre2, pre3);
         return this;
     }
 
     public <P1, P2, P3, P4> CompoundFuncBuilderN<Return> addEnd(IFunc4<P1, P2, P3, P4, Return> end, IFunc<P1> pre1, IFunc<P2> pre2, IFunc<P3> pre3, IFunc<P4> pre4) {
-        checkEnd();
+        checkEnd(end);
         mGraphBuilder.addLink(end, pre1, pre2, pre3, pre4);
         return this;
     }
 
     public <P1, P2, P3, P4, P5> CompoundFuncBuilderN<Return> addEnd(IFunc5<P1, P2, P3, P4, P5, Return> end, IFunc<P1> pre1, IFunc<P2> pre2, IFunc<P3> pre3, IFunc<P4> pre4, IFunc<P5> pre5) {
-        checkEnd();
+        checkEnd(end);
         mGraphBuilder.addLink(end, pre1, pre2, pre3, pre4, pre5);
         return this;
     }
 
     public <P1, P2, P3, P4, P5, P6> CompoundFuncBuilderN<Return> addEnd(IFunc6<P1, P2, P3, P4, P5, P6, Return> end, IFunc<P1> pre1, IFunc<P2> pre2, IFunc<P3> pre3, IFunc<P4> pre4, IFunc<P5> pre5, IFunc<P6> pre6) {
-        checkEnd();
+        checkEnd(end);
         mGraphBuilder.addLink(end, pre1, pre2, pre3, pre4, pre5, pre6);
         return this;
     }
 
     public <P1, P2, P3, P4, P5, P6, P7> CompoundFuncBuilderN<Return> addEnd(IFunc7<P1, P2, P3, P4, P5, P6, P7, Return> end, IFunc<P1> pre1, IFunc<P2> pre2, IFunc<P3> pre3, IFunc<P4> pre4, IFunc<P5> pre5, IFunc<P6> pre6, IFunc<P7> pre7) {
-        checkEnd();
+        checkEnd(end);
         mGraphBuilder.addLink(end, pre1, pre2, pre3, pre4, pre5, pre6, pre7);
         return this;
     }
 
     public <P1, P2, P3, P4, P5, P6, P7, P8> CompoundFuncBuilderN<Return> addEnd(IFunc8<P1, P2, P3, P4, P5, P6, P7, P8, Return> end, IFunc<P1> pre1, IFunc<P2> pre2, IFunc<P3> pre3, IFunc<P4> pre4, IFunc<P5> pre5, IFunc<P6> pre6, IFunc<P7> pre7, IFunc<P8> pre8) {
-        checkEnd();
+        checkEnd(end);
         mGraphBuilder.addLink(end, pre1, pre2, pre3, pre4, pre5, pre6, pre7, pre8);
         return this;
     }
 
     public <P1, P2, P3, P4, P5, P6, P7, P8, P9> CompoundFuncBuilderN<Return> addEnd(IFunc9<P1, P2, P3, P4, P5, P6, P7, P8, P9, Return> end, IFunc<P1> pre1, IFunc<P2> pre2, IFunc<P3> pre3, IFunc<P4> pre4, IFunc<P5> pre5, IFunc<P6> pre6, IFunc<P7> pre7, IFunc<P8> pre8, IFunc<P9> pre9) {
-        checkEnd();
+        checkEnd(end);
         mGraphBuilder.addLink(end, pre1, pre2, pre3, pre4, pre5, pre6, pre7, pre8, pre9);
         return this;
     }
 
     public <P1, P2, P3, P4, P5, P6, P7, P8, P9, P10> CompoundFuncBuilderN<Return> addEnd(IFunc10<P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, Return> end, IFunc<P1> pre1, IFunc<P2> pre2, IFunc<P3> pre3, IFunc<P4> pre4, IFunc<P5> pre5, IFunc<P6> pre6, IFunc<P7> pre7, IFunc<P8> pre8, IFunc<P9> pre9, IFunc<P10> pre10) {
-        checkEnd();
+        checkEnd(end);
         mGraphBuilder.addLink(end, pre1, pre2, pre3, pre4, pre5, pre6, pre7, pre8, pre9, pre10);
         return this;
     }
 
     public CompoundFuncBuilderN<Return> addEnd(IFuncN<Return> end, IFunc... preFuncs) {
-        checkEnd();
+        checkEnd(end);
         mGraphBuilder.addLink(end, preFuncs);
         return this;
     }
@@ -107,61 +107,73 @@ public class CompoundFuncBuilderN<Return> extends CompoundFuncBuilder {
      */
 
     public CompoundFuncBuilderN<Return> addNormal(IFunc0 end, IFunc<Void> pre1) {
+        checkNormal(end);
         mGraphBuilder.addLink(end, pre1);
         return this;
     }
 
     public <P> CompoundFuncBuilderN<Return> addNormal(IFunc1<P, ?> end, IFunc<P> pre1) {
+        checkNormal(end);
         mGraphBuilder.addLink(end, pre1);
         return this;
     }
 
     public <P1, P2> CompoundFuncBuilderN<Return> addNormal(IFunc2<P1, P2, ?> end, IFunc<P1> pre1, IFunc<P2> pre2) {
+        checkNormal(end);
         mGraphBuilder.addLink(end, pre1, pre2);
         return this;
     }
 
     public <P1, P2, P3> CompoundFuncBuilderN<Return> addNormal(IFunc3<P1, P2, P3, ?> end, IFunc<P1> pre1, IFunc<P2> pre2, IFunc<P3> pre3) {
+        checkNormal(end);
         mGraphBuilder.addLink(end, pre1, pre2, pre3);
         return this;
     }
 
     public <P1, P2, P3, P4> CompoundFuncBuilderN<Return> addNormal(IFunc4<P1, P2, P3, P4, ?> end, IFunc<P1> pre1, IFunc<P2> pre2, IFunc<P3> pre3, IFunc<P4> pre4) {
+        checkNormal(end);
         mGraphBuilder.addLink(end, pre1, pre2, pre3, pre4);
         return this;
     }
 
     public <P1, P2, P3, P4, P5> CompoundFuncBuilderN<Return> addNormal(IFunc5<P1, P2, P3, P4, P5, ?> end, IFunc<P1> pre1, IFunc<P2> pre2, IFunc<P3> pre3, IFunc<P4> pre4, IFunc<P5> pre5) {
+        checkNormal(end);
         mGraphBuilder.addLink(end, pre1, pre2, pre3, pre4, pre5);
         return this;
     }
 
     public <P1, P2, P3, P4, P5, P6> CompoundFuncBuilderN<Return> addNormal(IFunc6<P1, P2, P3, P4, P5, P6, ?> end, IFunc<P1> pre1, IFunc<P2> pre2, IFunc<P3> pre3, IFunc<P4> pre4, IFunc<P5> pre5, IFunc<P6> pre6) {
+        checkNormal(end);
         mGraphBuilder.addLink(end, pre1, pre2, pre3, pre4, pre5, pre6);
         return this;
     }
 
     public <P1, P2, P3, P4, P5, P6, P7> CompoundFuncBuilderN<Return> addNormal(IFunc7<P1, P2, P3, P4, P5, P6, P7, ?> end, IFunc<P1> pre1, IFunc<P2> pre2, IFunc<P3> pre3, IFunc<P4> pre4, IFunc<P5> pre5, IFunc<P6> pre6, IFunc<P7> pre7) {
+        checkNormal(end);
         mGraphBuilder.addLink(end, pre1, pre2, pre3, pre4, pre5, pre6, pre7);
         return this;
     }
 
     public <P1, P2, P3, P4, P5, P6, P7, P8> CompoundFuncBuilderN<Return> addNormal(IFunc8<P1, P2, P3, P4, P5, P6, P7, P8, ?> end, IFunc<P1> pre1, IFunc<P2> pre2, IFunc<P3> pre3, IFunc<P4> pre4, IFunc<P5> pre5, IFunc<P6> pre6, IFunc<P7> pre7, IFunc<P8> pre8) {
+        checkNormal(end);
         mGraphBuilder.addLink(end, pre1, pre2, pre3, pre4, pre5, pre6, pre7, pre8);
         return this;
     }
 
     public <P1, P2, P3, P4, P5, P6, P7, P8, P9> CompoundFuncBuilderN<Return> addNormal(IFunc9<P1, P2, P3, P4, P5, P6, P7, P8, P9, ?> end, IFunc<P1> pre1, IFunc<P2> pre2, IFunc<P3> pre3, IFunc<P4> pre4, IFunc<P5> pre5, IFunc<P6> pre6, IFunc<P7> pre7, IFunc<P8> pre8, IFunc<P9> pre9) {
+        checkNormal(end);
         mGraphBuilder.addLink(end, pre1, pre2, pre3, pre4, pre5, pre6, pre7, pre8, pre9);
         return this;
     }
 
     public <P1, P2, P3, P4, P5, P6, P7, P8, P9, P10> CompoundFuncBuilderN<Return> addNormal(IFunc10<P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, ?> end, IFunc<P1> pre1, IFunc<P2> pre2, IFunc<P3> pre3, IFunc<P4> pre4, IFunc<P5> pre5, IFunc<P6> pre6, IFunc<P7> pre7, IFunc<P8> pre8, IFunc<P9> pre9, IFunc<P10> pre10) {
+        checkNormal(end);
         mGraphBuilder.addLink(end, pre1, pre2, pre3, pre4, pre5, pre6, pre7, pre8, pre9, pre10);
         return this;
     }
 
     public CompoundFuncBuilderN<Return> addNormal(IFuncN end, IFunc... preFuncs) {
+        checkNormal(end);
         mGraphBuilder.addLink(end, preFuncs);
         return this;
     }
