61ced2a178b57bff97ff3aa9e02ac17b4e2c11a7
xyhuangjinfu <630569027@qq.com>
2020-04-03 14:02:27 +0800

    change lock

--- library/src/main/java/cn/hjf/taskflow/execute/TaskRunnablePool.java
+++ library/src/main/java/cn/hjf/taskflow/execute/TaskRunnablePool.java
@@ -2,6 +2,8 @@ package cn.hjf.taskflow.execute;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 import cn.hjf.taskflow.core.Task;
 
@@ -10,44 +12,60 @@ import cn.hjf.taskflow.core.Task;
  */
 class TaskRunnablePool {
 
+    private static Lock sLock = new ReentrantLock();
     private static Map<Session, Map<Long, TaskRunnable>> sTaskRunnableMap = new HashMap<>();
 
-    public static synchronized TaskRunnable getOrCreate(Session session, Task task) {
-        TaskRunnable r;
+    public static TaskRunnable getOrCreate(Session session, Task task) {
+        sLock.lock();
+        try {
+            TaskRunnable r;
 
-        if (sTaskRunnableMap.containsKey(session)) {
-            Map<Long, TaskRunnable> map = sTaskRunnableMap.get(session);
-            if (map.containsKey(task.getId())) {
-                r = map.get(task.getId());
+            if (sTaskRunnableMap.containsKey(session)) {
+                Map<Long, TaskRunnable> map = sTaskRunnableMap.get(session);
+                if (map.containsKey(task.getId())) {
+                    r = map.get(task.getId());
+                } else {
+                    r = new TaskRunnable(session, task);
+                    map.put(task.getId(), r);
+                }
             } else {
+                Map<Long, TaskRunnable> map = new HashMap<>();
                 r = new TaskRunnable(session, task);
                 map.put(task.getId(), r);
+
+                sTaskRunnableMap.put(session, map);
             }
-        } else {
-            Map<Long, TaskRunnable> map = new HashMap<>();
-            r = new TaskRunnable(session, task);
-            map.put(task.getId(), r);
 
-            sTaskRunnableMap.put(session, map);
+            return r;
+        } finally {
+            sLock.unlock();
         }
-
-        return r;
     }
 
-    public static synchronized void remove(Session session, Task task) {
-        if (!sTaskRunnableMap.containsKey(session)) {
-            return;
+    public static void remove(Session session, Task task) {
+        sLock.lock();
+        try {
+            if (!sTaskRunnableMap.containsKey(session)) {
+                return;
+            }
+
+            Map<Long, TaskRunnable> map = sTaskRunnableMap.get(session);
+            map.remove(task.getId());
+
+            if (map.isEmpty()) {
+                sTaskRunnableMap.remove(session);
+            }
+        } finally {
+            sLock.unlock();
         }
+    }
 
-        Map<Long, TaskRunnable> map = sTaskRunnableMap.get(session);
-        map.remove(task.getId());
-
-        if (map.isEmpty()) {
+    public static void remove(Session session) {
+        sLock.lock();
+        try {
             sTaskRunnableMap.remove(session);
+        } finally {
+            sLock.unlock();
         }
-    }
-
-    public static synchronized void remove(Session session) {
-        sTaskRunnableMap.remove(session);
     }
 }
